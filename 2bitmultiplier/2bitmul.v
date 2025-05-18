942f89aee9abb82133396b3bd387a29438772202
HarshathKumar14 <mharshathkumar14@gmail.com>
2025-05-18 11:47:12 +0530

    Update 2bitmul.v

--- 2bitmultiplier/2bitmul.v
+++ 2bitmultiplier/2bitmul.v
@@ -1,23 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 18.05.2025 08:22:18
-// Design Name: 
-// Module Name: 2bitmul
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module twobitmul(
