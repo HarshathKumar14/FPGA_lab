6ea1e995f404e656e09a1852ebdfc13fc236ee6e
HarshathKumar14 <mharshathkumar14@gmail.com>
2025-05-18 11:47:41 +0530

    Update mux.v

--- MUX/mux.v
+++ MUX/mux.v
@@ -1,23 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.05.2025 21:15:11
-// Design Name: 
-// Module Name: test_ckt
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module test_ckt(
