7a18560fff08c18673ee17e31c43be59eb276d16
HarshathKumar14 <mharshathkumar14@gmail.com>
2025-05-18 11:47:29 +0530

    Update FullAdder.v

--- FullAdder/FullAdder.v
+++ FullAdder/FullAdder.v
@@ -1,23 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 18.05.2025 08:03:40
-// Design Name: 
-// Module Name: FullAdder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module FullAdder(
